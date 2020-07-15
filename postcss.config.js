75b53c66bd15acabde82a1ecee48f6733b7e7149
Aakash Goel <goelaakash79@gmail.com>
2020-07-16 03:05:44 +0530

    :package: update: removed unused css

--- postcss.config.js
+++ postcss.config.js
@@ -3,15 +3,15 @@ const tailwindcss = require("tailwindcss");
 module.exports = {
 	plugins: [
 		tailwindcss("./tailwind.config.js"),
-		require("autoprefixer")
-		// require("@fullhuman/postcss-purgecss")({
-		// 	content: [
-		// 		"./src/components/*.js",
-		// 		"./public/index.html",
-		// 		"./src/App.js"
-		// 	],
-		// 	defaultExtractor: content =>
-		// 		content.match(/[A-Za-z0-9-_:/]+/g) || []
-		// })
+		require("autoprefixer"),
+		require("@fullhuman/postcss-purgecss")({
+			content: [
+				"./src/components/*.js",
+				"./public/index.html",
+				"./src/App.js"
+			],
+			defaultExtractor: content =>
+				content.match(/[A-Za-z0-9-_:/]+/g) || []
+		})
 	]
 };
