1e76a443130549256fafada0666c41c560080897
Aakash Goel <goelaakash79@gmail.com>
2020-07-16 01:24:51 +0530

    :package: update: removed unused css :octocat:

--- postcss.config.js
+++ postcss.config.js
@@ -4,15 +4,14 @@ module.exports = {
 	plugins: [
 		tailwindcss("./tailwind.config.js"),
 		require("autoprefixer"),
-		process.env.NODE_ENV === "production" &&
-			require("@fullhuman/postcss-purgecss")({
-				content: [
-					"./src/components/*.js",
-					"./public/index.html",
-					"./src/App.js",
-				],
-				defaultExtractor: (content) =>
-					content.match(/[A-Za-z0-9-_:/]+/g) || [],
-			}),
-	],
+		require("@fullhuman/postcss-purgecss")({
+			content: [
+				"./src/components/*.js",
+				"./public/index.html",
+				"./src/App.js"
+			],
+			defaultExtractor: content =>
+				content.match(/[A-Za-z0-9-_:/]+/g) || []
+		})
+	]
 };
